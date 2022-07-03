68496db795335aee5f088da79609e7dd3d0ee408
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2022-07-03 13:42:51 +0530

    Update 004Permutations.cpp

--- 3. December/Lecture 14 - 11thDecember2021/004Permutations.cpp
+++ 3. December/Lecture 14 - 11thDecember2021/004Permutations.cpp
@@ -32,7 +32,7 @@ void generatePermutations(char* inp, int i, set<string>& s) {
 
 	for(int j=i; j<strlen(inp); j++) {
 		swap(inp[i], inp[j]);
-		generatePermutations(inp, i+1, s);
+		generatePermutations(inp, i+1, s); // ask your friend for i+1
 		swap(inp[i], inp[j]); // backtracking
 	}
 }
